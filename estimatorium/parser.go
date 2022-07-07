9f5ad32b57a94257479cc4fffcc92bb681441741
xonix <xonixx@gmail.com>
2022-07-07 16:27:22 +0300

    Come up with format for input file and create parser for it #8

--- estimatorium/parser.go
+++ estimatorium/parser.go
@@ -1,15 +1,19 @@
 package estimatorium
 
+import (
+	"regexp"
+	"strings"
+)
+
 // 1. each directive can go at most one time
 
-type directiveParsed struct {
-	directive string
-	value     string
-	values    map[string]string
+type directiveVals struct {
+	value  string
+	values map[string]string
 }
 
 type projParsed struct {
-	directives   map[string]directiveParsed // each directive can go at most one time
+	directives   map[string]directiveVals // each directive can go at most one time
 	tasksRecords []taskRecord
 }
 
@@ -30,7 +34,6 @@ const (
 type directiveDef struct {
 	name          string
 	directiveType directiveType
-	//parse(line string) (directiveParsed, error)
 }
 
 var directives = []directiveDef{
@@ -46,6 +49,37 @@ var directives = []directiveDef{
 	{"team", DtKeyVal},
 }
 
-func ProjectFromString(projData string) Project {
+var spaceRe = regexp.MustCompile("[ \t]")
 
+func ProjectFromString(projData string) Project {
+	return Project{} // TODO
+}
+
+func parseProj(projData string) projParsed {
+	projParsed := projParsed{}
+	var parsedDirectives map[string]directiveVals
+	lines := strings.Split(projData, "\n")
+	for _, line := range lines {
+		parts := spaceRe.Split(line, 2)
+		if parts[0] == "tasks" {
+			break
+		}
+		for _, directive := range directives {
+			if parts[0] == directive.name {
+				if directive.directiveType == DtSingleValue {
+					parsedDirectives[directive.name] = directiveVals{value: strings.TrimSpace(parts[1])}
+				} else if directive.directiveType == DtKeyVal {
+					valParts := spaceRe.Split(parts[1], -1)
+					values := map[string]string{}
+					for _, valPart := range valParts {
+						keyVal := strings.SplitN(valPart, "=", 2)
+						values[keyVal[0]] = values[keyVal[1]]
+					}
+					parsedDirectives[directive.name] = directiveVals{values: values}
+				}
+			}
+		}
+	}
+	projParsed.directives = parsedDirectives
+	return projParsed
 }
